119b0094e6b2565d38b47341cbe3d1229a6eab29
danielwalbridge <danielwalbridge@gmail.com>
2022-12-13 14:52:50 -0500

    server app controller added, test method for eureka server and the ability for the client to get the host information

---
+++ src/main/java/com/example/serverapp/controller/ServerController.java
@@ -0,0 +1,38 @@
+package com.example.serverapp.controller;
+
+import com.example.serverapp.communication.ServerCommunication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/server")
+public class ServerController {
+
+    @Autowired
+    private ServerCommunication serverCommunication;
+
+    @GetMapping("/test")
+    public String getData() {
+        return "from SERVER-SERVICE " + serverCommunication.getConsoleInfo();
+    }
+
+    @GetMapping("/host")
+    public String geHostData() {
+        System.out.println("getHostData Method being called");
+
+        InetAddress ip = null;
+        String hostname = "";
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "Host Name of Server is : " + hostname + "ip of Server is : " + ip ;
+    }
+}
