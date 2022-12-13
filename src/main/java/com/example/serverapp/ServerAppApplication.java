dc2e0f8c1a67749765c9b304a917b471e9a6edbf
danielwalbridge <danielwalbridge@gmail.com>
2022-12-13 14:52:06 -0500

    initial serverApp sectup

---
+++ src/main/java/com/example/serverapp/ServerAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAppApplication.class, args);
+    }
+
+}
